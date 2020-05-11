1908c1b01df5fa459a91e3265d82625b01545904
AlexeyPerm <fx5600@yandex.ru>
2020-05-11 15:13:37 +0500

    Убрал дичь

--- 05_basic_scripts/task_5_2.py
+++ 05_basic_scripts/task_5_2.py
@@ -19,23 +19,24 @@ Mask:
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
 ip, network = input('Введите ip-сеть в формате ip/network: ').split('/')
+
 bin_network = '1' * int(network) + '0' * (32 - int(network))
-oct1, oct2, oct3, oct4 = ip.split('.')
-first_oct_m = int(bin_network[0:8], 2)
-second_oct_m = int(bin_network[8:16], 2)
-third_oct_m = int(bin_network[16:24], 2)
-fourth_oct_m = int(bin_network[24:], 2)
+IP = ip.split('.')
 
+mask_oct1 = int(bin_network[0:8], 2)
+mask_oct2 = int(bin_network[8:16], 2)
+mask_oct3 = int(bin_network[16:24], 2)
+mask_oct4 = int(bin_network[24:], 2)
 
-result = (f"Network:\n"
-          f"{oct1:<10}{oct2:<10}{oct3:<10}{oct4:<10}\n"
-          f"{int(oct1):08b}  {int(oct2):08b}  {int(oct3):08b}  {int(oct4):08b}\n"
-          f"\n"
-          f"Mask:\n"
-          f"/{network}\n"
-          f"{first_oct_m:<10}{second_oct_m:<10}{third_oct_m:<10}{fourth_oct_m:<10}\n"
-          f"{first_oct_m:<10b}{second_oct_m:<10b}{third_oct_m:<10b}{fourth_oct_m:<10b}"
-          )
+result = (f'Network:\n'
+          f'{int(IP[0]):<10}{int(IP[1]):<10}{int(IP[2]):<10}{int(IP[3]):<10}\n'
+          f'{int(IP[0]):08b}  {int(IP[1]):08b}  {int(IP[2]):08b}  {int(IP[3]):08b}\n'
+          f'\n'
+          f'Mask:\n'
+          f'/{network}\n'
+          f'{int(mask_oct1):<10}{int(mask_oct2):<10}{int(mask_oct3):<10}{int(mask_oct4):<10}\n'
+          f'{int(mask_oct1):08b}  {int(mask_oct2):08b}  {int(mask_oct3):08b}  {int(mask_oct4):08b}\n')
 
 print(result)
