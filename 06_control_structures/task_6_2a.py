3e88ff994eb2077f3f32baec0ba9eea4a18f3349
AlexeyPerm <fx5600@yandex.ru>
2020-05-12 14:23:05 +0500

    done 6.2a

--- 06_control_structures/task_6_2a.py
+++ 06_control_structures/task_6_2a.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 6.2a
 
 Сделать копию скрипта задания 6.2.
@@ -13,4 +13,32 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
-'''
+"""
+
+ip = input('Ввериде ip-адрес: ')
+correct = True
+
+if ip.count('.') != 3:
+    correct = False
+elif correct:
+    for k in ip.split('.'):
+        if int(k) not in range(0, 256):
+            correct = False
+            break
+
+
+
+if correct:
+    first_byte = int(ip.split('.')[0])
+    if 1 <= first_byte <= 223:
+        print('unicast')
+    elif 224 <= first_byte <= 239:
+        print('multicast')
+    elif ip == '255.255.255.255':
+        print('local broadcast')
+    elif ip == '0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
+else:
+    print('Неправильный IP-адрес')
