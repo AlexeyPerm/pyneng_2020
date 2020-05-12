c5e7a38089cec692763071fd1f8647b3c0a2c4ff
AlexeyPerm <fx5600@yandex.ru>
2020-05-12 14:55:23 +0500

    Недоделал

--- 06_control_structures/task_6_2b.py
+++ 06_control_structures/task_6_2b.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 6.2b
 
 Сделать копию скрипта задания 6.2a.
@@ -8,4 +8,31 @@
 Если адрес был введен неправильно, запросить адрес снова.
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
-'''
+"""
+
+ip = input('Ввериде ip-адрес: ')
+correct = True
+
+while not correct:
+    if ip.count('.') != 3:
+        correct = False
+    elif correct:
+        for k in ip.split('.'):
+            if int(k) not in range(0, 256):
+                correct = False
+                break
+
+if correct:
+    first_byte = int(ip.split('.')[0])
+    if 1 <= first_byte <= 223:
+        print('unicast')
+    elif 224 <= first_byte <= 239:
+        print('multicast')
+    elif ip == '255.255.255.255':
+        print('local broadcast')
+    elif ip == '0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
+else:
+    print('Неправильный IP-адрес')
