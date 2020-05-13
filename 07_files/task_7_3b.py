49b3736d8e9a2dd5ff4aad230d7151b9b41ff689
AlexeyPerm <fx5600@yandex.ru>
2020-05-13 21:06:25 +0500

    done 7.3b

--- 07_files/task_7_3b.py
+++ 07_files/task_7_3b.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 7.3b
 
 Сделать копию скрипта задания 7.3a.
@@ -10,4 +10,18 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
-'''
+"""
+
+result = []
+
+with open('CAM_table.txt') as src:
+    for line in src:
+        if 'DYNAMIC' in line:
+            a = line.split()
+            a[0] = int(a[0])
+            result.append(a)
+
+a = int(input('Введите номер влана: '))
+for k in result:
+    if k[0] == a:
+        print(f' {k[0]:<9}{k[1]}   {k[3]}')
