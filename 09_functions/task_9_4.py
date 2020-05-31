2ff72e4db3d37a509ffc70e4071e8d1011134bfb
AlexeyPerm <fx5600@yandex.ru>
2020-05-31 12:20:06 +0500

    done 9.4

--- 09_functions/task_9_4.py
+++ 09_functions/task_9_4.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 9.4
 
 Создать функцию convert_config_to_dict, которая обрабатывает конфигурационный файл коммутатора и возвращает словарь:
 * Все команды верхнего уровня (глобального режима конфигурации), будут ключами.
-* Если у команды верхнего уровня есть подкоманды, они должны быть в значении у соответствующего ключа, в виде списка (пробелы в начале строки надо удалить).
+* Если у команды верхнего уровня есть подкоманды, они должны быть в значении у соответствующего ключа, в виде списка
+(пробелы в начале строки надо удалить).
 * Если у команды верхнего уровня нет подкоманд, то значение будет пустым списком
 
 У функции должен быть один параметр config_filename, который ожидает как аргумент имя конфигурационного файла.
@@ -18,13 +19,14 @@
 
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
-'''
+"""
+from pprint import pprint
 
 ignore = ['duplex', 'alias', 'Current configuration']
 
 
 def ignore_command(command, ignore):
-    '''
+    """
     Функция проверяет содержится ли в команде слово из списка ignore.
 
     command - строка. Команда, которую надо проверить
@@ -33,5 +35,24 @@ def ignore_command(command, ignore):
     Возвращает
     * True, если в команде содержится слово из списка ignore
     * False - если нет
-    '''
+    """
     return any(word in command for word in ignore)
+
+
+def convert_config_to_dict(config_filename):
+    result = {}
+    with open(config_filename) as f:
+        f.readline()
+        for line in f:
+            if ignore_command(line, ignore) or line.startswith('!'):
+                pass
+            elif not line.startswith(' '):
+                key = line.strip()
+                result[key] = []
+            else:
+                result[key].append(line.strip())
+
+    return result
+
+
+print(convert_config_to_dict('config_sw1.txt'))
